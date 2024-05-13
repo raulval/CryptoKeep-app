258d3c4cd8d5e3d3fbb288422defdf5ea1eead42
Raul Val <raulval@outlook.com>
2024-05-13 16:19:15 -0300

    feat: add options to change currency view in settings

--- src/components/BottomSheetSettings/index.tsx
+++ src/components/BottomSheetSettings/index.tsx
@@ -7,6 +7,7 @@ import { colors } from "@/theme/colors";
 import { useTranslation } from "react-i18next";
 import CountryFlag from "react-native-country-flag";
 import { LanguageEnum, useLanguageStore } from "@/store/languageStore";
+import { useCurrencyStore } from "@/store/currencyStore";
 
 export type Props = {
   onClose: () => void;
@@ -17,6 +18,7 @@ export const BottomSheetSettings = forwardRef<Bottom, Props>(
     const { t, i18n } = useTranslation("settings");
     const { theme: colorTheme } = useThemeStore();
     const { setLanguage } = useLanguageStore();
+    const { currencyCode, setCurrencyCode } = useCurrencyStore();
     const languageSwitch = (language: LanguageEnum) => {
       setLanguage(language);
       i18n.changeLanguage(language);
@@ -81,6 +83,114 @@ export const BottomSheetSettings = forwardRef<Bottom, Props>(
                   <MaterialIcons
                     name={
                       i18n.language === LanguageEnum.PT
+                        ? "radio-button-on"
+                        : "radio-button-off"
+                    }
+                    size={24}
+                    color={colors.primary}
+                  />
+                </Pressable>
+              </View>
+            </View>
+          </View>
+
+          <View className="mt-6 gap-4">
+            <Text className="text-light-text dark:text-dark-text font-semibold text-[18px]">
+              {t("Currency")}
+            </Text>
+            <View className="gap-4">
+              <View className="flex-row items-center gap-4">
+                <CountryFlag isoCode="us" size={20} />
+                <Pressable
+                  className="flex-1 justify-between flex-row"
+                  onPress={() => setCurrencyCode("USD")}
+                >
+                  <Text className="text-light-text dark:text-dark-text font-normal text-[16px]">
+                    {t("Dollar")}
+                  </Text>
+                  <MaterialIcons
+                    name={
+                      currencyCode === "USD"
+                        ? "radio-button-on"
+                        : "radio-button-off"
+                    }
+                    size={24}
+                    color={colors.primary}
+                  />
+                </Pressable>
+              </View>
+              <View className="flex-row items-center gap-4">
+                <CountryFlag isoCode="br" size={20} />
+                <Pressable
+                  className="flex-1 justify-between flex-row"
+                  onPress={() => setCurrencyCode("BRL")}
+                >
+                  <Text className="text-light-text dark:text-dark-text font-normal text-[16px]">
+                    {t("Brazilian Real")}
+                  </Text>
+                  <MaterialIcons
+                    name={
+                      currencyCode === "BRL"
+                        ? "radio-button-on"
+                        : "radio-button-off"
+                    }
+                    size={24}
+                    color={colors.primary}
+                  />
+                </Pressable>
+              </View>
+              <View className="flex-row items-center gap-4">
+                <CountryFlag isoCode="eu" size={20} />
+                <Pressable
+                  className="flex-1 justify-between flex-row"
+                  onPress={() => setCurrencyCode("EUR")}
+                >
+                  <Text className="text-light-text dark:text-dark-text font-normal text-[16px]">
+                    {t("Euro")}
+                  </Text>
+                  <MaterialIcons
+                    name={
+                      currencyCode === "EUR"
+                        ? "radio-button-on"
+                        : "radio-button-off"
+                    }
+                    size={24}
+                    color={colors.primary}
+                  />
+                </Pressable>
+              </View>
+              <View className="flex-row items-center gap-4">
+                <CountryFlag isoCode="gb" size={20} />
+                <Pressable
+                  className="flex-1 justify-between flex-row"
+                  onPress={() => setCurrencyCode("GBP")}
+                >
+                  <Text className="text-light-text dark:text-dark-text font-normal text-[16px]">
+                    {t("Pound Sterling")}
+                  </Text>
+                  <MaterialIcons
+                    name={
+                      currencyCode === "GBP"
+                        ? "radio-button-on"
+                        : "radio-button-off"
+                    }
+                    size={24}
+                    color={colors.primary}
+                  />
+                </Pressable>
+              </View>
+              <View className="flex-row items-center gap-4">
+                <CountryFlag isoCode="mx" size={20} />
+                <Pressable
+                  className="flex-1 justify-between flex-row"
+                  onPress={() => setCurrencyCode("MXN")}
+                >
+                  <Text className="text-light-text dark:text-dark-text font-normal text-[16px]">
+                    {t("Mexican Peso")}
+                  </Text>
+                  <MaterialIcons
+                    name={
+                      currencyCode === "MXN"
                         ? "radio-button-on"
                         : "radio-button-off"
                     }
