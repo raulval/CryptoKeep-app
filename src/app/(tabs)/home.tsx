23585c0e85be944afcf80cfc713ac725e37d5e2d
Raul Val <raulval@outlook.com>
2024-10-11 19:27:55 -0300

    feat: add wallets in home page

--- src/app/(tabs)/home.tsx
+++ src/app/(tabs)/home.tsx
@@ -3,15 +3,24 @@ import { HeaderHome } from "@/components/HeaderHome";
 import { MainCard } from "@/components/MainCard";
 import { useRef } from "react";
 import Bottom from "@gorhom/bottom-sheet";
-import { Platform, SafeAreaView, Text, View } from "react-native";
+import { Platform, Pressable, SafeAreaView, Text, View } from "react-native";
 import { useTranslation } from "react-i18next";
-import { PortfolioCards } from "@/components/PortfolioCards";
+import { PortfolioList } from "@/components/PortfolioList";
+import { BottomSheetAddWallet } from "@/components/BottomSheetAddWallet";
+import { WalletsList } from "@/components/WalletsList";
 
 export default function Home() {
-  const bottomSheetRef = useRef<Bottom>(null);
-  const handleBottomSheetOpen = () => bottomSheetRef.current?.expand();
-  const handleBottomSheetClose = () => bottomSheetRef.current?.snapToIndex(0);
   const { t } = useTranslation("home");
+  const bottomSheetSettingsRef = useRef<Bottom>(null);
+  const handleBottomSheetSettingsOpen = () =>
+    bottomSheetSettingsRef.current?.expand();
+  const handleBottomSheetSettingsClose = () =>
+    bottomSheetSettingsRef.current?.snapToIndex(0);
+  const bottomSheetAddWalletRef = useRef<Bottom>(null);
+  const handleBottomSheetAddWalletOpen = () =>
+    bottomSheetAddWalletRef.current?.expand();
+  const handleBottomSheetAddWalletClose = () =>
+    bottomSheetAddWalletRef.current?.snapToIndex(0);
   return (
     <SafeAreaView
       className="flex-1 bg-light-background dark:bg-dark-background"
@@ -20,18 +29,40 @@ export default function Home() {
       }}
     >
       <View className="gap-[32px]">
-        <HeaderHome onOpenSettingsBottomSheet={handleBottomSheetOpen} />
+        <HeaderHome onOpenSettingsBottomSheet={handleBottomSheetSettingsOpen} />
         <MainCard />
         <View className="flex flex-col gap-4">
           <Text className="px-[24px] text-[18px] font-bold text-light-text dark:text-dark-text">
             {t("My Portfolio")}
           </Text>
-          <PortfolioCards />
+          <PortfolioList />
+        </View>
+        <View className="flex flex-col gap-4">
+          <View className="flex flex-row items-center justify-between pr-[18px] mb-4">
+            <Text className="px-[24px] text-[18px] font-bold text-light-text dark:text-dark-text">
+              {t("My Wallets")}
+            </Text>
+            <Pressable
+              className={`w-[125px] h-[40px] bg-primary rounded-[16px] flex items-center justify-center`}
+              onPress={handleBottomSheetAddWalletOpen}
+            >
+              <Text
+                className={`text-[14px] font-medium text-center text-dark-text`}
+              >
+                {t("Add wallet")}
+              </Text>
+            </Pressable>
+          </View>
+          <WalletsList />
         </View>
       </View>
       <BottomSheetSettings
-        ref={bottomSheetRef}
-        onClose={handleBottomSheetClose}
+        ref={bottomSheetSettingsRef}
+        onClose={handleBottomSheetSettingsClose}
+      />
+      <BottomSheetAddWallet
+        ref={bottomSheetAddWalletRef}
+        onClose={handleBottomSheetAddWalletClose}
       />
     </SafeAreaView>
   );
