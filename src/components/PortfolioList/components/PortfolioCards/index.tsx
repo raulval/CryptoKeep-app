fc1433a9182fa8785a71749849472489b9b46a7f
Raul Val <raulval@outlook.com>
2024-11-06 16:16:00 -0300

    feat: implement portfolio listing from database

--- src/components/PortfolioList/components/PortfolioCards/index.tsx
+++ src/components/PortfolioList/components/PortfolioCards/index.tsx
@@ -3,7 +3,11 @@ import {
   EActionToConvertCryptoOrFiat,
 } from "@/common/helpers/convertCryptoOrFiat";
 import { formatCryptoBalance } from "@/common/helpers/formatCryptoBalance";
+import { getCryptoCoinId } from "@/common/helpers/getCryptoCoinId";
+import { getCryptoNetwork } from "@/common/helpers/getCryptoNetwork";
+import { getCryptoSymbol } from "@/common/helpers/getCryptoSymbol";
 import useGetQuotation from "@/services/api.config.coingecko";
+import { IPortfolioDB } from "@/services/database/portfolio/usePortfolioDatabase";
 import { useCurrencyStore } from "@/store/currencyStore";
 import { useLanguageStore } from "@/store/languageStore";
 import { useThemeStore } from "@/store/themeStore";
@@ -12,7 +16,7 @@ import { View, ActivityIndicator, Text } from "react-native";
 import Icon from "react-native-ico-cryptocurrency";
 
 interface PortfolioCardsProps {
-  item: { crypto: string; cryptoCurrency: string; amount: number };
+  item: IPortfolioDB;
 }
 
 export const PortfolioCards = ({ item }: PortfolioCardsProps) => {
@@ -20,7 +24,7 @@ export const PortfolioCards = ({ item }: PortfolioCardsProps) => {
   const { currencyCode } = useCurrencyStore();
   const { language } = useLanguageStore();
   const { data, isLoading } = useGetQuotation(
-    [item.crypto.toLowerCase()],
+    [getCryptoCoinId(item.network)],
     currencyCode
   );
 
@@ -30,7 +34,9 @@ export const PortfolioCards = ({ item }: PortfolioCardsProps) => {
       return <ActivityIndicator size="small" color={colors.primary} />;
     } else if (priceExists) {
       return convertCryptoOrFiat({
-        value: item.amount,
+        value: Number(
+          formatCryptoBalance(item.network, item.total_balance ?? "0")
+        ),
         action: EActionToConvertCryptoOrFiat.CRYPTO_TO_FIAT,
         quotation: data[0].current_price,
         localeInfo: { currencyCode, languageTag: language },
@@ -57,13 +63,17 @@ export const PortfolioCards = ({ item }: PortfolioCardsProps) => {
       }
     >
       <View className="flex flex-row items-center gap-2">
-        <Icon name={item.cryptoCurrency.toLowerCase()} height={24} width={24} />
+        <Icon
+          name={getCryptoSymbol(item.network).toLowerCase()}
+          height={24}
+          width={24}
+        />
         <View className="flex flex-col justify-center">
-          <Text className="text-[12px] font-normal text-light-text2 dark:text-dark-text2">
-            {item.crypto}
+          <Text className="text-[12px] font-normal text-light-text2 dark:text-dark-text2 capitalize">
+            {getCryptoNetwork(item.network)}
           </Text>
           <Text className="text-[12px] font-bold text-light-text dark:text-dark-text">
-            {item.cryptoCurrency}
+            {getCryptoSymbol(item.network)}
           </Text>
         </View>
       </View>
@@ -72,7 +82,8 @@ export const PortfolioCards = ({ item }: PortfolioCardsProps) => {
           {loadingValue()}
         </Text>
         <Text className="text-[14px] text-end font-normal text-light-text2 dark:text-dark-text2">
-          {item.cryptoCurrency} {formatCryptoBalance(item.crypto, item.amount)}
+          {getCryptoSymbol(item.network)}{" "}
+          {formatCryptoBalance(item.network, item.total_balance ?? "0")}
         </Text>
       </View>
     </View>
--- src/components/PortfolioList/index.tsx
+++ src/components/PortfolioList/index.tsx
@@ -1,53 +1,50 @@
 import { FlashList } from "@shopify/flash-list";
-import { View } from "react-native";
+import { ActivityIndicator, Text, View } from "react-native";
 import React from "react";
 import { PortfolioCards } from "./components/PortfolioCards";
+import { usePortfolio } from "@/services/requests/portfolio/usePortfolio";
+import { colors } from "@/theme/colors";
+import { useTranslation } from "react-i18next";
+import { useSearchStore } from "@/store/searchStore";
 
 const ItemSeparator = () => <View className="w-4" />;
 
 export const PortfolioList = () => {
-  const portfolio = [
-    {
-      crypto: "Bitcoin",
-      cryptoCurrency: "BTC",
-      amount: 0.016,
-    },
-    {
-      crypto: "Ethereum",
-      cryptoCurrency: "ETH",
-      amount: 0.01,
-    },
-    {
-      crypto: "matic-network",
-      cryptoCurrency: "MATIC",
-      amount: 1.32455,
-    },
-    {
-      crypto: "Solana",
-      cryptoCurrency: "SOL",
-      amount: 10.345,
-    },
-    {
-      crypto: "Tether",
-      cryptoCurrency: "USDT",
-      amount: 50000.0,
-    },
-  ];
+  const { portfolio, isLoading } = usePortfolio();
+  const { search } = useSearchStore();
+  const { t } = useTranslation("home");
+
+  if (isLoading) {
+    return (
+      <View className="flex-1 justify-center items-center">
+        <ActivityIndicator size="large" color={colors.primary} />
+      </View>
+    );
+  }
   return (
-    <View className="h-fit">
-      <FlashList
-        data={portfolio}
-        horizontal={true}
-        showsHorizontalScrollIndicator={false}
-        ItemSeparatorComponent={ItemSeparator}
-        keyExtractor={(item) => item.cryptoCurrency}
-        estimatedItemSize={200}
-        contentContainerStyle={{
-          paddingHorizontal: 24,
-          paddingBottom: 8,
-        }}
-        renderItem={({ item }) => <PortfolioCards item={item} />}
-      />
+    <View>
+      {portfolio.length > 0 && !search && (
+        <View className="flex flex-col gap-4">
+          <Text className="px-[24px] text-[18px] font-bold text-light-text dark:text-dark-text">
+            {t("My Portfolio")}
+          </Text>
+          <View className="h-fit">
+            <FlashList
+              data={portfolio}
+              horizontal={true}
+              showsHorizontalScrollIndicator={false}
+              ItemSeparatorComponent={ItemSeparator}
+              keyExtractor={(item) => `${item.id}-${item.network}`}
+              estimatedItemSize={200}
+              contentContainerStyle={{
+                paddingHorizontal: 24,
+                paddingBottom: 8,
+              }}
+              renderItem={({ item }) => <PortfolioCards item={item} />}
+            />
+          </View>
+        </View>
+      )}
     </View>
   );
 };
